79823dd5934bff48dbd225a7e4363211838d76a5
defifofum <defifofum0@gmail.com>
2021-04-28 11:17:12 -0400

    feat: Add timelock contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,12 @@
 const GoldenBanana = artifacts.require("GoldenBanana");
 const Treasury = artifacts.require("Treasury");
+const Timelock = artifacts.require("Timelock");
 const MockBEP20 = artifacts.require("MockBEP20");
 const { getNetworkConfig } = require('../deploy.config')
 
 module.exports = async function(deployer,  network, accounts) {
     console.log(network)
-    let { bananaAddress } = getNetworkConfig(network, accounts);
+    let { bananaAddress, admin } = getNetworkConfig(network, accounts);
     const initialSupply = '3000000000000000000000000000'
 
     if (!bananaAddress) {
@@ -13,6 +14,7 @@ module.exports = async function(deployer,  network, accounts) {
         bananaAddress = MockBEP20.address
     }
 
+    await deployer.deploy(Timelock, admin, 43000); // 12 hours (6 hour minimum)
     await deployer.deploy(GoldenBanana, initialSupply);
     await deployer.deploy(Treasury, bananaAddress, GoldenBanana.address);
 
