ae02cbf034c2a834d41e26d30c33a2131ae94707
defifofum <defifofum0@gmail.com>
2021-10-12 17:03:25 -0400

    refactor: Update migration config

--- deploy.config.js
+++ deploy.config.js
@@ -10,7 +10,9 @@ function getNetworkConfig(network, accounts) {
         console.log(`Deploying with BSC testnet config.`)
         return {
             bananaAddress: '0x4Fb99590cA95fc3255D9fA66a1cA46c43C34b09a',
-            admin: '0xb5e1Ec9861D7c1C99cB3d79dd602cC6122F0d7dc'  // Apeguru
+            gnanaAddress: '0x9407026d236DEAE22CC1F3c419A9e47CBfCFE9E5',
+            // admin: '0xb5e1Ec9861D7c1C99cB3d79dd602cC6122F0d7dc'  // Apeguru
+            admin: '0xE375D169F8f7bC18a544a6e5e546e63AD7511581',  // Tastic
         }
     } else if (['development'].includes(network)) {
         console.log(`Deploying with development config.`)
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,7 @@ const { getNetworkConfig } = require('../deploy.config')
 
 module.exports = async function(deployer,  network, accounts) {
     console.log(network)
-    let { bananaAddress } = getNetworkConfig(network, accounts);
+    let { bananaAddress, gnanaAddress } = getNetworkConfig(network, accounts);
     const initialSupply = '3000000000000000000000000000'
 
     if (!bananaAddress) {
@@ -13,8 +13,12 @@ module.exports = async function(deployer,  network, accounts) {
         bananaAddress = MockBEP20.address
     }
 
-    await deployer.deploy(GoldenBanana, initialSupply);
-    await deployer.deploy(Treasury, bananaAddress, GoldenBanana.address);
+    if (!gnanaAddress) {
+        await deployer.deploy(GoldenBanana, initialSupply);
+        gnanaAddress = GoldenBanana.address;
+    }
+
+    await deployer.deploy(Treasury, bananaAddress, gnanaAddress);
 
     const goldenBanana = await GoldenBanana.deployed();
     await goldenBanana.excludeAccount(Treasury.address);
